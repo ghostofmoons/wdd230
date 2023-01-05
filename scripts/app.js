89039b0245ad75349c5cd9e03520ef08117d443e
ghostofmoons <ghostofmoons@hotmail.com>
2023-01-05 14:42:24 -0700

    Week01 Applied Javascript activity

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+//Get Current date
+const todayDate = new Date();
+
+//Get year from current date and use it next to copyright in footer
+document.getElementById("currentyear").innerHTML = todayDate.getFullYear();
+
+// set up todays date in footer
