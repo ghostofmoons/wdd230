9a846d8eebc97dba09bc41a88a369c0637be4d0a
ghostofmoons <ghostofmoons@hotmail.com>
2023-01-06 11:45:00 -0700

    Portal page update, weekly pages added

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,4 @@ const todayDate = new Date();
 //Get year from current date and use it next to copyright in footer
 document.getElementById("currentyear").innerHTML = todayDate.getFullYear();
 
-// set up todays date in footer
+//Get Date and time from todayDate and use it for last update
