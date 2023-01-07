0d507c524808dc9f175862f4b02196fb96099814
ghostofmoons <ghostofmoons@hotmail.com>
2023-01-06 17:35:44 -0700

    Update listing

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,15 @@ const todayDate = new Date();
 document.getElementById("currentyear").innerHTML = todayDate.getFullYear();
 
 //Get Date and time from todayDate and use it for last update
+const editDate = new Date(document.lastModified);
+
+const options = {
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+  weekday: "short",
+};
+document.querySelector("#lastchange").innerHTML = editDate.toLocaleDateString(
+  en - US,
+  options
+);
