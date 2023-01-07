5a4bad3d29be94e9ccfe7ad3282e89003ba647fd
ghostofmoons <ghostofmoons@hotmail.com>
2023-01-07 09:35:52 -0700

    fix in javascript

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
 const todayDate = new Date();
 
 //Get year from current date and use it next to copyright in footer
-document.getElementById("currentyear").innerHTML = todayDate.getFullYear();
+document.querySelector("#currentyear").innerHTML = todayDate.getFullYear();
 
 //Get Date and time from todayDate and use it for last update
 let editDate = document.lastModified;
