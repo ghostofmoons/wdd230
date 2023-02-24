3fc1b8ce1fa490db137754947719b6ba119852da
ghostofmoons <ghostofmoons@hotmail.com>
2023-02-23 21:10:23 -0700

    finish visit site stuff

--- Chamber/scripts/visits.js
+++ Chamber/scripts/visits.js
@@ -1,22 +1,42 @@
-//check to see if there is something stored
-//if there is no logged date say "This is your first visit"
-//store date
-//else get date and subtract from today's date
-//then say "It's beem 'date difference' since last visit"
-//get new date and replace old date
+
+
 
 let storeDate = localStorage.getItem("date");
 
 
-if (storeDate == null) {    
-    localStorage.setItem("date", todayDate.getDate());
-    document.querySelector(".visit").textContent = "This is your first visit";
+//if (storeDate == null) {    
+//    localStorage.setItem("date", todayDate.getDate());
+//    document.querySelector(".visit").textContent = "-This is your first visit-";
+//} else {
+//    let oldDate = localStorage.getItem("date");
+//    console.log(oldDate);
+//    let subDates = todayDate.getDate() - oldDate;
+//    let message = `- Your last visit was ${subDates} days ago -`;
+//    document.querySelector(".visit").innerHTML = message;
+//    console.log(subDates);
+//    localStorage.setItem("date", todayDate.getDate());
+//}
+
+//const todayDate = new Date()
+const date =  Date.now();
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+let staticDate = Math.round(date / day);
+console.log(staticDate) 
+
+if (storeDate == null) {       
+    localStorage.setItem("date", staticDate);
+    document.querySelector(".visit").textContent = "-This is your first visit-";
 } else {
-    let oldDate = localStorage.getItem("date");
-    console.log(oldDate);
-    let subDates = todayDate.getDate() - oldDate;
-    let message = `Your last visit was ${subDates} days ago`;
+    let oldStaticDate = localStorage.getItem("date");
+    console.log(oldStaticDate);
+    console.log(staticDate)    
+    let subDates = staticDate - oldStaticDate;
+    let message = `- Your last visit was ${subDates} days ago -`;
     document.querySelector(".visit").innerHTML = message;
+    
     console.log(subDates);
-    localStorage.setItem("date", todayDate.getDate());
+    localStorage.clear();
+    localStorage.setItem("date",staticDate);
 }
