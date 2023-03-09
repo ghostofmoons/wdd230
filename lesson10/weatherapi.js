afb6e2e0cb6b8f08d0b63dbe7237a2d698813d43
ghostofmoons <ghostofmoons@hotmail.com>
2023-03-09 11:56:10 -0700

    weather api lesson

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -1,3 +1,42 @@
+
+//varibles for what information we want
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+
+//Variable for conecting with API
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=f6db6bc304682febe0fa24a0e884ae0c";
+
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong> `
+  }
+
+  function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
